22d6c1e0831865e6febd541aac866fb76522b8e4
unknown <ssalonen@gmail.com>
2011-01-13 09:22:07 +0200

    Formatting changes

--- history_playlist_callback.cpp
+++ history_playlist_callback.cpp
@@ -11,22 +11,22 @@ void history_playlist_callback::on_playlist_activate(t_size p_old, t_size p_new)
 	static_api_ptr_t<playlist_history>()->on_playlist_activate(p_old, p_new);
 }
 
-void  history_playlist_callback::on_playlist_created (t_size p_index, const char *p_name, t_size p_name_len) {
+void history_playlist_callback::on_playlist_created (t_size p_index, const char *p_name, t_size p_name_len) {
 	TRACK_CALL_TEXT_DEBUG("history_playlist_callback::on_playlist_created");
 	static_api_ptr_t<playlist_history>()->on_playlist_created(p_index, p_name, p_name_len);
 }
 
-void  history_playlist_callback::on_playlists_reorder (const t_size *p_order, t_size p_count) {
+void history_playlist_callback::on_playlists_reorder (const t_size *p_order, t_size p_count) {
 	TRACK_CALL_TEXT_DEBUG("history_playlist_callback::on_playlists_reorder");
 	static_api_ptr_t<playlist_history>()->on_playlists_reorder(p_order, p_count);
 }
 
-void  history_playlist_callback::on_playlists_removing (const bit_array &p_mask, t_size p_old_count, t_size p_new_count) {
+void history_playlist_callback::on_playlists_removing (const bit_array &p_mask, t_size p_old_count, t_size p_new_count) {
 	TRACK_CALL_TEXT_DEBUG("history_playlist_callback::on_playlists_removing");
 	static_api_ptr_t<playlist_history>()->on_playlists_removing(p_mask, p_old_count, p_new_count);
 }
 
-void  history_playlist_callback::on_playlists_removed (const bit_array &p_mask, t_size p_old_count, t_size p_new_count) {
+void history_playlist_callback::on_playlists_removed (const bit_array &p_mask, t_size p_old_count, t_size p_new_count) {
 	TRACK_CALL_TEXT_DEBUG("history_playlist_callback::on_playlists_removed");
 	static_api_ptr_t<playlist_history>()->on_playlists_removed(p_mask, p_old_count, p_new_count);
 }
